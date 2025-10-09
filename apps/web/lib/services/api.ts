b4b8f8ce77d1d7cd3cbae69e5912b6c8e2786b0e
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2025-10-08 21:07:14 -0300

    `fix(Web)`: Fetch API status failing on build

--- apps/web/lib/services/api.ts
+++ apps/web/lib/services/api.ts
@@ -1,7 +1,12 @@
 import { axios } from "../auth/axios";
+import { tryCatch } from "../utils";
 
 export async function getApiHealthStatus() {
-    const res = await axios.get<"OK">("/");
+    const { data: res, error } = await tryCatch(axios.get<"OK">("/"));
+
+    if (error) return false;
+
     const healthy = res.status == 200 && res.data == "OK";
+
     return healthy;
 }
