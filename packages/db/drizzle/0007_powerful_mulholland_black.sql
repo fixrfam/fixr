d72d887c5a1e1dc3a716e3317334761a33b085e4
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2026-05-27 10:34:46 -0300

    refactor(DB): Compat column types to hold scraped data

---
+++ packages/db/drizzle/0007_powerful_mulholland_black.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `models` MODIFY COLUMN `announced` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `status` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `weight` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `sim` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `display_size` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `display_resolution` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `display_protection` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `price` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `display_size_ratio` varchar(255);--> statement-breakpoint
+ALTER TABLE `models` MODIFY COLUMN `released` varchar(255);
