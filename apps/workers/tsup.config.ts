2b2cb5a9cd20e0f35202cb90c090ef645df86ee5
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2026-05-27 00:33:29 -0300

    fix(CI): Workers build issue

--- apps/workers/tsup.config.ts
+++ apps/workers/tsup.config.ts
@@ -3,6 +3,19 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: "esm",
+	platform: "node",
+	target: "node22",
 	outExtension: () => ({ js: ".js" }),
-	noExternal: ["@fixr/mail", "@fixr/constants", "@fixr/env"],
+	noExternal: [
+		"@fixr/mail",
+		"@fixr/constants",
+		"@fixr/env",
+		"@react-email/render",
+		"@react-email/components",
+	],
+	esbuildOptions: (options) => {
+		options.banner = {
+			js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`,
+		};
+	},
 });
